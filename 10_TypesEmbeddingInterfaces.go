7d639ef8d47056c3b038b548ea3ebf6eda593b92
frosk <frosk@frosk.uio.no>
2016-10-20 14:53:45 +0200

    Add example for oop aspects
    Use types and values of the type.
    Embedding the type for inheritance and use composition.
    Shadow methods.
    Define a interface which is implemented by all three types.w

---
+++ 10_TypesEmbeddingInterfaces.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+type gopher struct {
+	color string
+}
+
+func (g gopher) sayHello() {
+	fmt.Printf("Hello I am a %s Gopher\n", g.color) 
+}
+
+type evilGopher struct {
+	gopher
+	evilHabit string
+}
+func (e evilGopher) doEvilStuff() {
+	e.sayHello()
+	fmt.Printf("The %s evil Gopher does: %s\n", e.gopher.color, e.evilHabit)
+}
+
+
+type lovelyGopher struct {
+  g	 gopher
+}
+func (l lovelyGopher) sayHello() {
+	fmt.Printf("Hello I am a lovely %s Gopher\n", l.g.color)
+}
+
+type helloer interface{
+	sayHello() 
+}
+
+func main() {
+	g1 := gopher{color: "blue"}
+	g2 := evilGopher{evilHabit: "not using gofmt", gopher: gopher{"pink"}}
+	g3 := lovelyGopher{gopher{"green"}}
+
+	var hellos = []helloer{g1, g2, g3}	
+	for _, h := range hellos {
+		h.sayHello()
+	}
+}
