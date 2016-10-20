c92685a614f18aefaabde1c0f06bd0264a025589
akwick <anna-katharina.wickert@mailbox.org>
2016-10-20 15:35:16 +0200

    EvilGopher is public.
    Use private and public structs in the example.

--- 10_TypesEmbeddingInterfaces.go
+++ 10_TypesEmbeddingInterfaces.go
@@ -10,12 +10,12 @@ func (g gopher) sayHello() {
 	fmt.Printf("Hello I am a %s Gopher\n", g.color)
 }
 
-type evilGopher struct {
+type EvilGopher struct {
 	gopher
 	evilHabit string
 }
 
-func (e evilGopher) doEvilStuff() {
+func (e EvilGopher) DoEvilStuff() {
 	e.sayHello()
 	fmt.Printf("The %s evil Gopher does: %s\n", e.gopher.color, e.evilHabit)
 }
@@ -34,12 +34,12 @@ type helloer interface {
 
 func main() {
 	g1 := gopher{color: "blue"}
-	g2 := evilGopher{evilHabit: "not using gofmt", gopher: gopher{"pink"}}
+	g2 := EvilGopher{evilHabit: "not using gofmt", gopher: gopher{"pink"}}
 	g3 := lovelyGopher{gopher{"green"}}
 
 	var hellos = []helloer{g1, g2, g3}
 	for _, h := range hellos {
 		h.sayHello()
 	}
-	g2.doEvilStuff()
+	g2.DoEvilStuff()
 }
